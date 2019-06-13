bc500ec6d9f1b6929b3c6c60f4300e02c2a622d6
davemc85 <davemcallister@hotmail.co.uk>
2019-06-13 11:47:10 +0100

    models created

--- src/main/java/com/codeclan/example/CourseBooking/models/Booking.java
+++ src/main/java/com/codeclan/example/CourseBooking/models/Booking.java
@@ -1,4 +1,31 @@
 package com.codeclan.example.CourseBooking.models;
 
 public class Booking {
+
+    private Long id;
+    private String date;
+
+
+    public Booking(String date) {
+        this.date = date;
+    }
+
+    public Booking() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
